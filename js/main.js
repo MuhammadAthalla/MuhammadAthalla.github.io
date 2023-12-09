208d7c6852f1c4c0335198605a5d76cbc2029564
MuhammadAthalla <juraganathalla@gmail.com>
2023-12-09 09:18:48 +0700

    Path 1

--- js/main.js
+++ js/main.js
@@ -6,3 +6,14 @@ toggle.onclick = function(){
     body.classList.toggle("active") 
     body.classList.toggle("darkmode") 
 }
+let myAnimation = anime({
+  targets: ".animejs",
+  keyframes: [
+    { translateY: -300 },
+   
+    { translateY: 2 },
+  ],
+  duration: 4000,
+  easing: "easeOutQuad(5, .100)",
+  loop: true,
+});
